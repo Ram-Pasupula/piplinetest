a8ac9ace279d7a5e10486282134fe96e7c684db5
RamPasupula <kris.0374@live.com>
2020-02-19 14:17:02 +0800

    base

---
+++ ws/prestoOra/pipeline/src/com/test/pipeline/TestPipeline.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.test.pipeline;
+
+/**
+ * @author mac
+ *
+ */
+public class TestPipeline {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("I am able to see the output!!");
+	}
+
+}
